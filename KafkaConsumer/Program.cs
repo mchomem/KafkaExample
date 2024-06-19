767174ad420d8e5afdc0558cb5763360ce803574
Misael C. Homem <misael.homem@gmail.com>
2024-06-19 01:35:19 -0300

    Add project files.

---
+++ KafkaConsumer/Program.cs
@@ -0,0 +1,30 @@
+﻿using Confluent.Kafka;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        var config = new ConsumerConfig
+        {
+            GroupId = "test-consumer-group",
+            BootstrapServers = "localhost:9092",
+            AutoOffsetReset = AutoOffsetReset.Earliest
+        };
+
+        using var consumer = new ConsumerBuilder<Ignore, string>(config).Build();
+        consumer.Subscribe("test-topic");
+
+        try
+        {
+            while (true)
+            {
+                var consumeResult = consumer.Consume();
+                Console.WriteLine($"Received message: {consumeResult.Message.Value}");
+            }
+        }
+        catch (OperationCanceledException)
+        {
+            consumer.Close();
+        }
+    }
+}
---
+++ KafkaProducer/Program.cs
@@ -0,0 +1,26 @@
+﻿using Confluent.Kafka;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        var config = new ProducerConfig
+        {
+            BootstrapServers = "localhost:9092",
+        };
+
+        using var producer = new ProducerBuilder<Null, string>(config).Build();
+
+        for (int i = 0; i < 10; i++)
+        {
+            var result = producer
+                .ProduceAsync("test-topic", new Message<Null, string> { Value = $"Message {i}" })
+                .GetAwaiter()
+                .GetResult();
+
+            Console.WriteLine($"Sent message to {result.TopicPartitionOffset}");
+        }
+
+        producer.Flush(TimeSpan.FromSeconds(10));
+    }
+}
